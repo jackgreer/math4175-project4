6acb9deb23d1e59831b10fe0a6b075ef23252bc2
Jack <jackg1@vt.edu>
2022-12-03 14:19:39 -0500

    done with parts 1-3

--- differential_spn16.py
+++ differential_spn16.py
@@ -6,12 +6,17 @@
 
 from tabulate import tabulate
 
-# This dict represents the S-box
-pi_s = {0b000: 0b110, 0b001: 0b101, 0b010: 0b001, 0b011: 0b000,\
-        0b100: 0b011, 0b101: 0b010, 0b110: 0b111, 0b111: 0b100}
-# This dict represents the inverse of the S-box
-pi_s_inv = {0b000: 0b011, 0b001: 0b010, 0b010: 0b101, 0b011: 0b100,\
-            0b100: 0b111, 0b101: 0b001, 0b110: 0b000, 0b111: 0b110}
+# There's gotta be a better way to do this data structure ngl
+# Doing a dict for clarity, consider a regular list
+pi_s = {0x0: 0xE, 0x1: 0x4, 0x2: 0xD, 0x3: 0x1,\
+        0x4: 0x2, 0x5: 0xF, 0x6: 0xB, 0x7: 0x8,\
+        0x8: 0x3, 0x9: 0xA, 0xA: 0x6, 0xB: 0xC,\
+        0xC: 0x5, 0xD: 0x9, 0xE: 0x0, 0xF: 0x7}
+
+pi_s_inv = {0x0: 0xE, 0x1: 0x3, 0x2: 0x4, 0x3: 0x8,\
+            0x4: 0x1, 0x5: 0xC, 0x6: 0xA, 0x7: 0xF,\
+            0x8: 0x7, 0x9: 0xD, 0xA: 0x9, 0xB: 0x6,\
+            0xC: 0xB, 0xD: 0x2, 0xE: 0x0, 0xF: 0x5}
 
 # 1. Make a difference distribution table, analogous to Difference Distribution Table given in
 # the slide 7 of section 4.4 in class notes, for the given S-box.
@@ -19,38 +24,26 @@ pi_s_inv = {0b000: 0b011, 0b001: 0b010, 0b010: 0b101, 0b011: 0b100,\
 # Define N_D(x', y') = set of (x, x*) in Del(x') such that y ^ y* = y'
 # e.g., the number of pairs with input difference equal to x' and output difference equal to y'
 # (for a given S-box)
-
-difference_distribution_table = [[0 for row in range(8)] for col in range(8)]
+difference_distribution_table = [[0 for row in range(16)] for col in range(16)]
 # We'll consider each x'
-for x_prime in range(8):
-    for x in range(8):
+for x_prime in range(16):
+    for x in range(16):
         x_star = x ^ x_prime
-
         y = pi_s[x]
         y_star = pi_s[x_star]
         y_prime = y ^ y_star
-        
         difference_distribution_table[x_prime][y_prime] += 1
 
 print(tabulate(difference_distribution_table,\
-                headers=[x for x in range(8)],\
+                headers=[x for x in range(16)],\
                     tablefmt="fancy_grid", showindex="always"))
-
 
 # 2. Consider plaintexts such that x ^ x* = x' = 000 001. Using your difference distribution table
 # from the previous problem, find three distribution trails Tr1, Tr2, Tr3, all starting from P6,
-# whereas Tr1 ending at the last bit of S32, that is at H6, the second trail Tr2 ending at the
+# whereeas Tr1 ending at the last bit of S32, that is at H6, the second trail Tr2 ending at the
 # last 2 bits of S32, that is at H5 and H6, and the third trail Tr3 ending at all three bits of S32,
 # that is, at H4, H5 and H6.
 # Sketch all these three trials clearly and upload them as pdf files.
 
-
 # Given a fixed x' = 000 001
 
-# We're considering the cases where
-# H = 000001 with probability 1/4
-# H = 000011 with probability 1/8
-# H = 000111 with probability 1/8
-# Out of our candidates, we have three eligible right 4 tuples:
-# x        | 
-# 
